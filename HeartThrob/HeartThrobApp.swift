c87ef8c205900995e7c8b45ebced056441ab273e
daviddoswell <team@daviddoswell.com>
2024-02-03 22:38:53 -0500

    Initial Commit

---
+++ HeartThrob/HeartThrobApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartThrobApp.swift
+//  HeartThrob
+//
+//  Created by David Doswell on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HeartThrobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
